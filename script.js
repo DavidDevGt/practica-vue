7c9e20c553c8f69af63d2ed765dede70bc63a27a
DavidDevGt <josuedavidvl18@gmail.com>
2023-09-22 16:19:29 -0600

    directivas basicas en vue.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hola desde la instancia app'
+        message: 'Hola desde la instancia app',
+        number: 10,
+        list: ['1', '2', '3'],
+        active: false
+    }
+});
+
+new Vue({
+    el: '#app2',
+    data: {
+        message2: 'Hola que tal mundo'
     }
 });
